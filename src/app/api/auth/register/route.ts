6b564073329605d7f3efd81d4b880e1a334c2e9a
Rihana Ries <rihanaries@Rihanas-MacBook-Pro.local>
2024-11-24 20:38:31 +1100

    Save before db overhaul

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -33,8 +33,8 @@ export async function POST(req: Request) {
         email,
         password: hashedPassword,
         username,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
       },
     })
 
