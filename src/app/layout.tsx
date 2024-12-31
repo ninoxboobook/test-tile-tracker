be3c30080e0a77acf66d54f6a2028081ee7b88fb
Rihana Ries <rihanaries@Rihanas-MacBook-Pro.local>
2025-01-01 02:56:42 +1100

    added GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { SessionProvider } from '@/components/providers/session-provider'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Test Tile Tracker',
@@ -20,6 +21,18 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/zmu6flc.css"></link>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CGNF8CNWSD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CGNF8CNWSD');
+          `}
+        </Script>
       </head>
       <body className="bg-sand antialiased">
         <SessionProvider session={session}>
