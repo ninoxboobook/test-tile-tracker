8afaca5fe44b2fe956e1fa4765cd104cd9b13d5d
Rihana Ries <rihanaries@Rihanas-MacBook-Pro.local>
2025-01-02 19:28:07 +1100

    text colour fix

--- src/components/ui/cookie-consent.tsx
+++ src/components/ui/cookie-consent.tsx
@@ -23,13 +23,13 @@ export function CookieConsentBanner() {
       onDecline={handleDecline}
       expires={365}
       containerClasses="px-8 shadow-lg"
-      style={{ backgroundColor: "#FDFAF7", fontFamily: "degular, sans-serif", color: "#B35844", alignItems: "center" }}
+      style={{ backgroundColor: "#FDFAF7", fontFamily: "degular, sans-serif", color: "#534943", alignItems: "center" }}
       contentStyle={{ margin: "24px 0"}}
       buttonStyle={{ fontFamily: "degular, sans-serif", fontWeight: "500", backgroundColor: "#B35844", color: "#FAF8F6", borderRadius: "6px", padding: "8px 16px", margin: "24px 0" }}
       declineButtonStyle={{ fontFamily: "degular, sans-serif", fontWeight: "500", backgroundColor: "#FAF8F6", color: "#B35844", borderColor: "#B35844", borderWidth: "1px", borderStyle: "solid", borderRadius: "6px", padding: "8px 16px", margin: "24px 12px 24px 0" }}
     >
       <p className="mb-2">This website uses cookies to enhance your experience. By continuing to use this site, you agree to our use of cookies.</p>
-      <p>You can read more in our <a href="/privacy-policy" className="underline text-brand">Privacy Policy</a></p>
+      <p>You can read more in our <a href="/privacy-policy" className="underline text-brand-dark">Privacy Policy</a></p>
     </CookieConsent>
   )
 }
